0df4beb10aae1965016e0d9fa64a0d349293b5c0
Robin van Maasakker <robin@dashed.nl>
2026-06-04 17:18:10 +0200

    refactor: move product + order endpoints to dashed-ecommerce-core

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Dashed\DashedMobileApi\Http\Controllers\Api\V1\AuthController;
-use Dashed\DashedMobileApi\Http\Controllers\Api\V1\OrderController;
 use Dashed\DashedMobileApi\Http\Controllers\Api\V1\DeviceController;
-use Dashed\DashedMobileApi\Http\Controllers\Api\V1\ProductController;
 use Dashed\DashedMobileApi\Http\Controllers\Api\V1\DashboardController;
 use Dashed\DashedMobileApi\Http\Controllers\Api\V1\ConversationController;
 
@@ -16,14 +14,6 @@ Route::prefix('api/v1')->group(function (): void {
     Route::middleware(['auth:sanctum', 'mobile.site'])->group(function (): void {
         Route::post('auth/logout', [AuthController::class, 'logout']);
         Route::get('me', [AuthController::class, 'me']);
-
-        Route::get('products', [ProductController::class, 'index'])->middleware('ability:products.read');
-        Route::get('products/{product}', [ProductController::class, 'show'])->middleware('ability:products.read');
-        Route::put('products/{product}', [ProductController::class, 'update'])->middleware('ability:products.write');
-
-        Route::get('orders', [OrderController::class, 'index'])->middleware('ability:orders.read');
-        Route::get('orders/{order}', [OrderController::class, 'show'])->middleware('ability:orders.read');
-        Route::patch('orders/{order}', [OrderController::class, 'update'])->middleware('ability:orders.write');
 
         Route::get('conversations', [ConversationController::class, 'index'])->middleware('ability:chat.read');
         Route::get('conversations/{conversation}/messages', [ConversationController::class, 'messages'])
